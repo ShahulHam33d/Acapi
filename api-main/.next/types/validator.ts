4e6eadc06c04594b7f70f6467ae524e6ade5bddc
Shah <shahulshah333@gmail.com>
2026-09-21 19:46:29 +0530

    updated file location

--- api-main/.next/types/validator.ts
+++ api-main/.next/types/validator.ts
@@ -110,24 +110,6 @@ type RouteHandlerConfig<Route extends AppRouteHandlerRoutes = AppRouteHandlerRou
   type __Unused = __Check
 }
 
-// Validate ../../app/(auth)/logout/route.ts
-{
-  type __IsExpected<Specific extends RouteHandlerConfig<"/logout">> = Specific
-  const handler = {} as typeof import("../../app/(auth)/logout/route.js")
-  type __Check = __IsExpected<typeof handler>
-  // @ts-ignore
-  type __Unused = __Check
-}
-
-// Validate ../../app/(auth)/me/route.ts
-{
-  type __IsExpected<Specific extends RouteHandlerConfig<"/me">> = Specific
-  const handler = {} as typeof import("../../app/(auth)/me/route.js")
-  type __Check = __IsExpected<typeof handler>
-  // @ts-ignore
-  type __Unused = __Check
-}
-
 // Validate ../../app/api/auth/login/route.ts
 {
   type __IsExpected<Specific extends RouteHandlerConfig<"/api/auth/login">> = Specific
@@ -141,6 +123,15 @@ type RouteHandlerConfig<Route extends AppRouteHandlerRoutes = AppRouteHandlerRou
 {
   type __IsExpected<Specific extends RouteHandlerConfig<"/api/auth/logout">> = Specific
   const handler = {} as typeof import("../../app/api/auth/logout/route.js")
+  type __Check = __IsExpected<typeof handler>
+  // @ts-ignore
+  type __Unused = __Check
+}
+
+// Validate ../../app/api/auth/me/route.ts
+{
+  type __IsExpected<Specific extends RouteHandlerConfig<"/api/auth/me">> = Specific
+  const handler = {} as typeof import("../../app/api/auth/me/route.js")
   type __Check = __IsExpected<typeof handler>
   // @ts-ignore
   type __Unused = __Check
